f63a9b1ab357dff7db8382835f1d9dd62ba1d786
juni <woajun@naver.com>
2022-05-08 16:08:22 +0900

    디렉터리 수정

---
+++ src/chapter1/theater/forward/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import plays from "./plays";
+import invoices from "./invoices";
+import { statement } from "./statement";
+
+const result = ref("");
+result.value = statement(invoices, plays);
+</script>
+<template>
+  <h1>챕터1. 극단 예제 - 책대로</h1>
+  <br />
+  <textarea :value="result" rows="10" cols="50"></textarea>
+</template>
