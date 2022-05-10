691afe1dd1fb17e09b4cd826dbadbfacb6e0d85c
juni <woajun@naver.com>
2022-05-10 19:30:10 +0900

    volumeCredits 문장 슬라이드하기

--- src/chapter1/theater/forward/statement.js
+++ src/chapter1/theater/forward/statement.js
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -10,6 +9,7 @@ export function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits = volumeCreditsFor(perf);
   }
