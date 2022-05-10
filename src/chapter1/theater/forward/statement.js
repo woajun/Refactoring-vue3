611506d001f70b7287cc676ee167404b8a0c22b4
juni <woajun@naver.com>
2022-05-10 19:25:50 +0900

    format 함수선언 바꾸기

--- src/chapter1/theater/forward/statement.js
+++ src/chapter1/theater/forward/statement.js
@@ -6,21 +6,21 @@ export function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits = volumeCreditsFor(perf);
 
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}(${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))}(${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   function volumeCreditsFor(aPerformance) {
