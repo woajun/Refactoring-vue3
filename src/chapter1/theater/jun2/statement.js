a08b9f1594dac36a4236198c10b0e8e1b44336ec
juni <woajun@naver.com>
2022-05-19 19:19:04 +0900

    jun2- calcAmount

--- src/chapter1/theater/jun2/statement.js
+++ src/chapter1/theater/jun2/statement.js
@@ -12,29 +12,10 @@ export function statement(invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    // 포인트를 적립한다.
+    thisAmount = calcAmount(play, thisAmount, perf);
     volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-    // 청구 내역을 출력한다.
     result += ` ${play.name}: ${format(thisAmount / 100)}(${
       perf.audience
     }석)\n`;
@@ -43,4 +24,25 @@ export function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+}
+
+function calcAmount(play, thisAmount, perf) {
+  switch (play.type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": // 희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return thisAmount;
 }
