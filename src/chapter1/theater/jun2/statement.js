7b44fda50abdc264bb64964f4946c2bf9675e95a
juni <woajun@naver.com>
2022-05-19 19:19:30 +0900

    jun2- format => usd

--- src/chapter1/theater/jun2/statement.js
+++ src/chapter1/theater/jun2/statement.js
@@ -2,11 +2,6 @@ export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -16,14 +11,20 @@ export function statement(invoice, plays) {
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-    result += ` ${play.name}: ${format(thisAmount / 100)}(${
-      perf.audience
-    }석)\n`;
+    result += ` ${play.name}: ${usd(thisAmount)}(${perf.audience}석)\n`;
     totalAmount += thisAmount;
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+}
+
+function usd(amount) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount / 100);
 }
 
 function calcAmount(play, thisAmount, perf) {
