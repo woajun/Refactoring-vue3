ad152b9ac39307575d509b3ec6d45d77761e4034
woajun <woajun@naver.com>
2022-05-21 14:02:58 +0900

    study - volumeCreditsFor

--- src/chapter1/theater/study/statement.js
+++ src/chapter1/theater/study/statement.js
@@ -19,10 +19,16 @@ export function statement(invoice, plays) {
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      if ("comedy" === playFor(perf).type)
-        volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += volumeCreditsFor(perf);
     }
+    return volumeCredits;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      volumeCredits += Math.floor(aPerformance.audience / 5);
     return volumeCredits;
   }
 
