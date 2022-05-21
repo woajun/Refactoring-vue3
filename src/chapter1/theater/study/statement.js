3e92f5dc9044f361d378b1c337906ba2b176f7de
woajun <woajun@naver.com>
2022-05-21 14:42:32 +0900

    study - statementData

--- src/chapter1/theater/study/statement.js
+++ src/chapter1/theater/study/statement.js
@@ -1,7 +1,8 @@
 export function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 
-  function renderPlainText(invoice, plays) {
+  function renderPlainText(statementData, invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
       result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
