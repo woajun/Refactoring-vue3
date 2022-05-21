03c6aeb4213fe4333c77e674a779a6504d7e0af5
woajun <woajun@naver.com>
2022-05-21 14:04:27 +0900

    study - totlaVolumeCredits

--- src/chapter1/theater/study/statement.js
+++ src/chapter1/theater/study/statement.js
@@ -17,11 +17,8 @@ export function statement(invoice, plays) {
   return result;
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
+    return invoice.performances //
+      .reduce((total, p) => total + volumeCreditsFor(p), 0);
   }
 
   function volumeCreditsFor(aPerformance) {
