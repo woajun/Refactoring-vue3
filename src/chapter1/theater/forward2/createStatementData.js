cc249e2cf037daf781d1c22dec87efb7491b6e5a
juni <woajun@naver.com>
2022-05-22 12:43:07 +0900

    forward2 - PerformanceCalculator get volumeCredits

--- src/chapter1/theater/forward2/createStatementData.js
+++ src/chapter1/theater/forward2/createStatementData.js
@@ -24,6 +24,13 @@ class PerformanceCalculator {
     }
     return result;
   }
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -56,11 +63,8 @@ export default function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
 
   function playFor(aPerformance) {
