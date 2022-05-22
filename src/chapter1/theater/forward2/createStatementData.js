b8a19558af3462d22177e97c5ee254992bfec24e
juni <woajun@naver.com>
2022-05-22 12:57:21 +0900

    forward2 - subclass volumeCrdits override

--- src/chapter1/theater/forward2/createStatementData.js
+++ src/chapter1/theater/forward2/createStatementData.js
@@ -7,11 +7,7 @@ class PerformanceCalculator {
     throw new Error("서브클래스에서 처리하돟록 설계되었습니다.");
   }
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -33,6 +29,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
