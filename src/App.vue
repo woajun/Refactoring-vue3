0a137cae7e95df4f71b015956252885d166b9f78
juni <woajun@naver.com>
2022-05-31 20:19:17 +0900

    6-1-Init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <index />
+  <div>
+    <template v-for="(chapter, key) of chapters" :key="key">
+      <button @click="currentView = key">{{ key }}</button>
+    </template>
+    <template v-for="(chapter, key) of chapters" :key="key">
+      <div v-show="currentView === key">
+        <component :is="chapter" />
+      </div>
+    </template>
+  </div>
 </template>
 
 <script setup>
-import index from "./chapter1/theater/jun2";
+import { ref } from 'vue';
+import chapter1 from "./chapter1/theater/jun2";
+import chapter6 from "./chapter6";
+
+const currentView = ref("챕터1");
+const chapters = {
+  챕터1: chapter1,
+  챕터6: chapter6
+}
 </script>
---
+++ src/chapter6/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <template v-for="(chapter, key) of chapters" :key="key">
+                <button @click="currentView = key">{{ key }}</button>
+            </template>
+            <template v-for="(chapter, key) of chapters" :key="key">
+                <div v-show="currentView === key">
+                    <component :is="chapter" />
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+import first from './6-1Extract-Function/Init.vue'
+
+
+const currentView = ref("챕터1");
+const chapters = {
+    "6-1": first,
+}
+</script>
