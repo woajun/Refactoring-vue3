d56f020f63cd61f2d6250b4e5dceb60cb35599dd
juni <woajun@naver.com>
2022-05-31 20:49:13 +0900

    6-1-forward printBanner()

--- src/chapter6/6-1Extract-Function/forward/Init.vue
+++ src/chapter6/6-1Extract-Function/forward/Init.vue
@@ -31,9 +31,7 @@ const Clock = {
 function printOwing(invoice) {
     let outstanding = 0;
 
-    console.log("*********************");
-    console.log("***** 고객 채무 *****");
-    console.log("*********************");
+    printBanner();
 
     // 미해결 채무(outstanding)를 계산한다.
     for (const o of invoice.orders) {
@@ -48,6 +46,11 @@ function printOwing(invoice) {
     console.log(`고객명: ${invoice.customer}`);
     console.log(`채무액: ${outstanding}`)
     console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
+}
 
+function printBanner() {
+    console.log("*********************");
+    console.log("***** 고객 채무 *****");
+    console.log("*********************");
 }
 </script>
