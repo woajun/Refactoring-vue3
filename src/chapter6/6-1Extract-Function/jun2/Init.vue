1df9eaef5bd81247d4fdd7a07c45666d60304b42
juni <woajun@naver.com>
2022-05-31 21:13:50 +0900

    6-1-jun2 printDetails()

--- src/chapter6/6-1Extract-Function/jun2/Init.vue
+++ src/chapter6/6-1Extract-Function/jun2/Init.vue
@@ -27,12 +27,15 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
     // 세부 사항을 출력한다.
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`)
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
-
-
+    printDetails(invoice, outstanding);
 }
+
+function printDetails(invoice, outstanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
 function printBanner() {
     console.log("*********************");
     console.log("***** 고객 채무 *****");
