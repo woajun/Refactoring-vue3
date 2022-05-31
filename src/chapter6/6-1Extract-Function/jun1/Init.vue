ec87203974d65437b3311586a737527ab38a54c8
juni <woajun@naver.com>
2022-05-31 20:34:47 +0900

    6-1-jun1 _ extract dueDateFor()

--- src/chapter6/6-1Extract-Function/jun1/Init.vue
+++ src/chapter6/6-1Extract-Function/jun1/Init.vue
@@ -35,14 +35,18 @@ function printOwing(invoice) {
     console.log("*********************");
 
     // 마감일 (dueDate)을 기록한다.
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    invoice.dueDate = dueDateFor(invoice);
 
     // 세부 사항을 출력한다.
     console.log(`고객명: ${invoice.customer}`);
     console.log(`채무액: ${outstandingFor(invoice)}`)
     console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
 
+    function dueDateFor() {
+        const today = Clock.today;
+        return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    }
     function outstandingFor(invoice) {
         let result = 0;
         for (const o of invoice.orders) {
