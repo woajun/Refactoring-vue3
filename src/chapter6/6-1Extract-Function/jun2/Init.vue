5a00080ce51e26575592a7372a8fa9f4f31981f4
juni <woajun@naver.com>
2022-05-31 21:16:17 +0900

    6-1-jun2 calculateOutstanding()

--- src/chapter6/6-1Extract-Function/jun2/Init.vue
+++ src/chapter6/6-1Extract-Function/jun2/Init.vue
@@ -13,16 +13,18 @@ const Clock = {
 }
 
 function printOwing(invoice) {
-    let outstanding = 0;
-
     printBanner();
+    const outstanding = calculateOutstanding(invoice);
+    recordDueDate(invoice);
+    printDetails(invoice, outstanding);
+}
 
+function calculateOutstanding(invoice) {
+    let outstanding = 0;
     for (const o of invoice.orders) {
         outstanding += o.amount;
     }
-
-    recordDueDate(invoice);
-    printDetails(invoice, outstanding);
+    return outstanding
 }
 
 function recordDueDate(invoice) {
