accae4391d649caf56a895212f382f0a69ce993d
juni <woajun@naver.com>
2022-06-02 20:32:56 +0900

    6-3-ex2 init

--- src/chapter6/6-3Extract-Variable/example2/Jun1.vue
+++ src/chapter6/6-3Extract-Variable/example2/Jun1.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>{{ doSomething(order) }}</div>
+  <div>{{ order.price }}</div>
 </template>
 <script setup>
-const order = {
-  quantity: 50,
-  itemPrice: 1000,
-};
-function doSomething(order) {
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    );
+  }
 }
+const record = { quantity: 3, itemPrice: 700 };
+const order = new Order(record);
 </script>
