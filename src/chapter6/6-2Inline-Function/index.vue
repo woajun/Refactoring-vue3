561318a71272ee3605d81bfc7fb4b883bc3d3ffc
juni <woajun@naver.com>
2022-06-02 19:28:26 +0900

    6-2 디렉토리 정리

--- src/chapter6/6-2Inline-Function/index.vue
+++ src/chapter6/6-2Inline-Function/index.vue
@@ -14,18 +14,20 @@
 </template>
 <script setup>
 import { ref } from "vue";
-import init from "./init/Init.vue";
-import forward from "./forward/Init.vue";
-import forward2 from "./forward2/Init.vue";
-import jun1 from "./jun1/Init.vue";
+import ex1Init from "./example1/Init.vue";
+import ex1Jun1 from "./example1/Jun1.vue";
+import ex1Forward1 from "./example1/Forward.vue";
+import ex2Init from "./example2/Init.vue";
+import ex2Forward1 from "./example2/Forward.vue";
 // import jun2 from "./jun2/Init.vue";
 
 const currentView = ref("챕터1");
 const chapters = {
-  init: init,
-  forward: forward,
-  forward2: forward2,
-  jun1: jun1,
+  ex1Init: ex1Init,
+  ex1Forward1: ex1Forward1,
+  ex1Jun1: ex1Jun1,
+  ex2Init: ex2Init,
+  ex2Forward1: ex2Forward1,
   // "jun2": jun2,
 };
 </script>
