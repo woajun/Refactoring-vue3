034dcb27d90e201988cc5a057ce6cd86aa7ef05c
juni <woajun@naver.com>
2022-06-02 20:42:50 +0900

    6-4 init

---
+++ src/chapter6/6-4Inline-Variable/Init.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ apple() }}</div>
+</template>
+<script setup>
+const anOrder = { basePrice: 1200 };
+function apple() {
+  let basePrice = anOrder.basePrice;
+  return basePrice > 1000;
+}
+</script>
--- src/chapter6/index.vue
+++ src/chapter6/index.vue
@@ -6,10 +6,12 @@ import Tab from "@/components/Tab.vue";
 import one from "./6-1Extract-Function";
 import two from "./6-2Inline-Function";
 import three from "./6-3Extract-Variable";
+import four from "./6-4Inline-Variable";
 
 const chapters = {
   "6-1": one,
   "6-2": two,
   "6-3": three,
+  "6-4": four,
 };
 </script>
