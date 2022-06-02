c885066dd68f7f0b6f7492a503694ef7733797b5
juni <woajun@naver.com>
2022-06-02 19:10:15 +0900

    6-2-jun1 inline

--- src/chapter6/6-2Inline-Function/index.vue
+++ src/chapter6/6-2Inline-Function/index.vue
@@ -16,14 +16,14 @@
 import { ref } from "vue";
 import init from "./init/Init.vue";
 // import forward from "./forward/Init.vue";
-// import jun1 from "./jun1/Init.vue";
+import jun1 from "./jun1/Init.vue";
 // import jun2 from "./jun2/Init.vue";
 
 const currentView = ref("챕터1");
 const chapters = {
   init: init,
   // "forward": forward,
-  // "jun1": jun1,
+  jun1: jun1,
   // "jun2": jun2,
 };
 </script>
