237528e31ce3975fb22d55a195e8e7d8b807970e
juni <woajun@naver.com>
2022-06-08 19:01:49 +0900

    6-5-example1

---
+++ src/chapter6/6-5Change-Function-Declaration/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Tab :chapters="chapters"></Tab>
+</template>
+<script setup>
+import Tab from "@/components/Tab.vue";
+import Init from "./Init.vue";
+import Forward from "./Forward.vue";
+
+const chapters = {
+  Init,
+  Forward,
+};
+</script>
