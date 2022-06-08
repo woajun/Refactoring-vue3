ecb164b1ae96acadca04398733f7cd14dccb0626
juni <woajun@naver.com>
2022-06-08 18:55:41 +0900

    6-1 ~ 6-4 복습

---
+++ src/chapter6/6-2Inline-Function/example2/Jun2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>6.2함수 인라인하기 - 예제2</h1>
+    <br />
+    {{ reportLines(customer) }}
+  </div>
+</template>
+<script setup>
+import { reactive } from "@vue/reactivity";
+
+const customer = reactive({
+  name: "홍길동",
+  location: "잠실",
+});
+
+function reportLines(aCustomer) {
+  const lines = [];
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
+  return lines;
+}
+
+</script>
---
+++ src/chapter6/6-4Inline-Variable/Jun1.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ apple() }}</div>
+</template>
+<script setup>
+const anOrder = { basePrice: 1200 };
+function apple() {
+  return anOrder.basePrice > 1000;
+}
+</script>
