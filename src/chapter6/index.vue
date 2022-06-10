58f066601d910ca9df172cfe0d343017bb081904
juni <woajun@naver.com>
2022-06-10 19:02:05 +0900

    add sections

--- src/chapter6/index.vue
+++ src/chapter6/index.vue
@@ -1,5 +1,10 @@
 <template>
   <Tab :chapters="chapters"></Tab>
+  <template v-for= "(section,i) in sections" :key="i">
+    <template v-for="(example,i) in section" :key="i">
+      <button @click="example.method()">{{example.name}}</button>
+    </template> 
+  </template>
 </template>
 <script setup>
 import Tab from "@/components/Tab.vue";
@@ -9,6 +14,7 @@ import three from "./6-3Extract-Variable";
 import four from "./6-4Inline-Variable";
 import five from "./6-5Change-Function-Declaration";
 import eight from "./6-8Introduce-Parameter-Object";
+import nine from "./6-9Combine-Functions-into-Class";
 
 const chapters = {
   "6-1": one,
@@ -18,4 +24,6 @@ const chapters = {
   "6-5": five,
   "6-8": eight,
 };
+
+const sections = [nine];
 </script>
