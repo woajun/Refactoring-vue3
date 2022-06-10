5c2b3b61933edaba58c22bf3cb48b4a34011d412
juni <woajun@naver.com>
2022-06-10 19:20:31 +0900

    6-9-solution client2 use Reading class

--- src/chapter6/6-9Combine-Functions-into-Class/solution.js
+++ src/chapter6/6-9Combine-Functions-into-Class/solution.js
@@ -32,9 +32,12 @@ export default () => {
   }
 
   function client2() {
-    const aReading = acquireReading();
-    const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-    const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+    const rawReading = acquireReading();
+    const aReading = new Reading(rawReading);
+    const taxableCharge = Math.max(
+      0,
+      aReading.baseCharge - taxThreshold(aReading.year)
+    );
     return taxableCharge;
   }
 
