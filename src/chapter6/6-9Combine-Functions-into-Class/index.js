244c0df3f6174d937867e7f7d6cf833d11d6e106
juni <woajun@naver.com>
2022-06-10 19:05:20 +0900

    6-9-solution init

--- src/chapter6/6-9Combine-Functions-into-Class/index.js
+++ src/chapter6/6-9Combine-Functions-into-Class/index.js
@@ -1,7 +1,7 @@
 import example from "./ex";
-// import solution from "./solution";
+import solution from "./solution";
 
 export default [
   { name: "예제", method: example },
-  //   { name: "답", method: solution },
+  { name: "풀이", method: solution },
 ];
