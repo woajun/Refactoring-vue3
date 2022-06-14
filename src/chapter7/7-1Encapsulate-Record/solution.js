02d4a5db820a2744c65702d76c61b53be41d2966
juni <woajun@naver.com>
2022-06-14 21:46:45 +0900

    7-1-solution use getter

--- src/chapter7/7-1Encapsulate-Record/solution.js
+++ src/chapter7/7-1Encapsulate-Record/solution.js
@@ -1,14 +1,18 @@
 export default () => {
   const organization = { name: "애크미 구스베리", country: "GB" };
 
+  function getRawDataOfOrganization() {
+    return organization;
+  }
+
   function print() {
     const newName = "토니 민수 제이슨";
     let result;
-    result = `이름: ${organization.name}`;
+    result = `이름: ${getRawDataOfOrganization().name}`;
     console.log(result);
 
     organization.name = newName;
-    result = `이름: ${organization.name}`;
+    result = `이름: ${getRawDataOfOrganization().name}`;
     console.log(result);
   }
   print();
