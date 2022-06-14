ef2195de0ea78cf4ce88b28640ff61057b526914
juni <woajun@naver.com>
2022-06-14 18:51:16 +0900

    6-11-solution add calculatePricingData

--- src/chapter6/6-11Split-Phase/solution.js
+++ src/chapter6/6-11Split-Phase/solution.js
@@ -1,18 +1,18 @@
 import { chocolate, shippingMethod } from "./conditions";
 export default () => {
   function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+    const price = applyShipping(priceData, shippingMethod);
+    return price;
+  }
+
+  function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount =
       Math.max(quantity - product.discountThreshold, 0) *
       product.basePrice *
       product.discountRate;
-    const priceData = {
-      basePrice: basePrice,
-      quantity: quantity,
-      discount: discount,
-    };
-    const price = applyShipping(priceData, shippingMethod);
-    return price;
+    return { basePrice: basePrice, quantity: quantity, discount: discount };
   }
 
   function applyShipping(priceData, shippingMethod) {
