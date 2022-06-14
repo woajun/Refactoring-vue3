bdad20a3781853a3bb7f6631c7c7e2021f38f416
juni <woajun@naver.com>
2022-06-14 18:36:21 +0900

    6-11-solution extract function

--- src/chapter6/6-11Split-Phase/solution.js
+++ src/chapter6/6-11Split-Phase/solution.js
@@ -6,6 +6,11 @@ export default () => {
       Math.max(quantity - product.discountThreshold, 0) *
       product.basePrice *
       product.discountRate;
+    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    return price;
+  }
+
+  function applyShipping(basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase =
       basePrice > shippingMethod.discountThreshold
         ? shippingMethod.discountedFee
