9d55da55af4c7d28e9995b3e087e7548749f3220
juni <woajun@naver.com>
2022-06-18 20:33:34 +0900

    ch6 new index, old index

---
+++ src/chapter6/_index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Tab from "@/components/Tab.vue";
+import one from "./6-1Extract-Function";
+import two from "./6-2Inline-Function";
+import three from "./6-3Extract-Variable";
+import four from "./6-4Inline-Variable";
+import five from "./6-5Change-Function-Declaration";
+import eight from "./6-8Introduce-Parameter-Object";
+import nine from "./6-9Combine-Functions-into-Class";
+import ten from "./6-10Combine-Functions-into-Transform";
+import eleven from "./6-11Split-Phase";
+
+const chapters = {
+  "6-1": one,
+  "6-2": two,
+  "6-3": three,
+  "6-4": four,
+  "6-5": five,
+  "6-8": eight,
+};
+
+const sections = { "6-9": nine, "6-10": ten, "6-11": eleven };
+
+function executeExample(example) {
+  console.log(`====='${example.name}' 실행=====`);
+  example.method();
+}
+</script>
+<template>
+  <Tab :chapters="chapters"></Tab>
+  <template v-for="(section, key) in sections" :key="key">
+    <br /><br />{{ key }} :
+    <template v-for="(example, i) in section" :key="i">
+      <button @click="executeExample(example)">{{ example.name }}</button>&nbsp;
+    </template>
+  </template>
+</template>
