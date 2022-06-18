56a7953fb256dda371b39c5d1182c706546d965d
juni <woajun@naver.com>
2022-06-18 20:19:46 +0900

    ch1 merge output to executor

--- src/chapter1/condition/executor.js
+++ src/chapter1/condition/executor.js
@@ -1,6 +1,5 @@
 import plays from "./plays";
 import invoices from "./invoices";
-import expect from "./output";
 
 export default class Executor {
   constructor(name, statement) {
@@ -11,6 +10,12 @@ export default class Executor {
     return this.statement ? this.statement(invoices, plays) : undefined;
   }
   get expect() {
-    return expect;
+    return `청구 내역 (고객명: BigCo)
+ Hamlet: $650.00(55석)
+ As You Like It: $580.00(35석)
+ Othello: $500.00(40석)
+총액: $1,730.00
+적립 포인트: 47점
+`;
   }
 }
