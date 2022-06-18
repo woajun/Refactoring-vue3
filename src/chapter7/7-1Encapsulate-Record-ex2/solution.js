038dd3d52319da835175e417cbf2231c20761d51
juni <woajun@naver.com>
2022-06-18 16:46:58 +0900

    7-1-2 하던중인거 같은데 확인좀

--- src/chapter7/7-1Encapsulate-Record-ex2/solution.js
+++ src/chapter7/7-1Encapsulate-Record-ex2/solution.js
@@ -1,14 +1,23 @@
 import rawCustomerData from "./condition";
 export default () => {
+  class CustomerData {
+    constructor(data) {
+      this._data = data;
+    }
+  }
   let customerData = JSON.parse(JSON.stringify(rawCustomerData));
 
+  // eslint-disable-next-line no-unused-vars
+  function getCustomerData() {
+    return customerData;
+  }
   function getRawDataOfCustomers() {
     return customerData;
   }
 
   // eslint-disable-next-line no-unused-vars
   function setRawDataOfCustomers(arg) {
-    customerData = arg;
+    customerData = new CustomerData(arg);
   }
 
   function compareUsage(customerID, laterYear, month) {
@@ -18,13 +27,13 @@ export default () => {
     return { laterAmount: later, change: later - earlier };
   }
 
-  function writeAmount(customerID, year, month, amount) {
+  function setUsage(customerID, year, month, amount) {
     getRawDataOfCustomers()[customerID].usages[year][month] = amount;
   }
 
   function print() {
     console.log(compareUsage(1920, 2016, 2));
-    writeAmount(1920, 2016, 2, 100);
+    setUsage(1920, 2016, 2, 100);
     console.log(compareUsage(1920, 2016, 2));
   }
 
