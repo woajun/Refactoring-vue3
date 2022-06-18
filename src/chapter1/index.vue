29777ba9bfe141119dc0190a049f46c3df8373c4
juni <woajun@naver.com>
2022-06-18 17:04:07 +0900

    ch1 add test

--- src/chapter1/index.vue
+++ src/chapter1/index.vue
@@ -1,30 +1,39 @@
 <script setup>
 import plays from "./condition/plays";
 import invoices from "./condition/invoices";
-import output from "./condition/output";
+import expect from "./condition/output";
 import example from "./example/statement.js";
 import { ref } from "vue";
 const statements = { 문제: example };
 
 const result = ref("");
+const test = ref(true);
 
 const doSomething = function (statement) {
   result.value = statement(invoices, plays);
-  console.log(output);
+  test.value = result.value === expect ? true : true;
 };
 </script>
 <template>
+  <br />
   <template v-for="(statement, key) in statements" :key="key">
     <button @click="doSomething(statement)">{{ key }}</button>&nbsp;
   </template>
-  <textarea v-model="result" rows="10" cols="50"></textarea>
+  <br />
+  <br />
+  <textarea
+    :class="[test ? 'success' : 'fail']"
+    v-model="result"
+    rows="10"
+    cols="50"
+  ></textarea>
 </template>
 
 <style>
-.warning {
-  background-color: red;
-}
 .success {
   background-color: green;
+}
+.fail {
+  background-color: red;
 }
 </style>
