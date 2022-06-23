adfa71ae1e4a8ca9a2edf3c8943696ba058f37ea
juni <woajun@naver.com>
2022-06-23 21:07:09 +0900

    global executor

--- src/chapter6/1_extract-function/index.js
+++ src/chapter6/1_extract-function/index.js
@@ -1,4 +1,9 @@
 import example from "./example";
-import Executor from "./condition/executor.js";
+import solution from "./solution";
+import Executor from "../executor.js";
+import invoice from "./condition/invoice.js";
 
-export default [new Executor("예제", example, example)];
+export default [
+  new Executor("예제", example(invoice), example(invoice)),
+  new Executor("풀이", solution(invoice), example(invoice)),
+];
---
+++ src/chapter6/executor.js
@@ -0,0 +1,7 @@
+export default class Executor {
+  constructor(buttonName, result, expect) {
+    this.name = buttonName;
+    this.result = result;
+    this.expect = expect;
+  }
+}
