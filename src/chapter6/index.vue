05ff5da93a18b15d065e304c903d0d81e2e1e1ee
juni <woajun@naver.com>
2022-06-23 21:17:20 +0900

    6-2 init

--- src/chapter6/index.vue
+++ src/chapter6/index.vue
@@ -1,8 +1,12 @@
 <script setup>
 import one from "./1_extract-function";
+import two from "./2_inline-function";
 import Chapter from "@/components/Chapter.vue";
 
-const sections = { "6-1 예제": one };
+const sections = {
+  "6-1 예제": one,
+  "6.2 함수 인라인하기 Inline Function": two,
+};
 </script>
 <template>
   <chapter :sections="sections" />
