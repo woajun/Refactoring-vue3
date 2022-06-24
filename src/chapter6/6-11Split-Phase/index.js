25bc677a359b5db275157d11ce24619066a09e74
juni <woajun@naver.com>
2022-06-24 13:21:42 +0900

    remove unused files

--- src/chapter6/6-11Split-Phase/index.js
+++
@@ -1,9 +0,0 @@
-import example from "./ex";
-import solution from "./solution";
-// import try1 from "./try1";
-
-export default [
-  { name: "예제", method: example },
-  { name: "풀이", method: solution },
-  // { name: "연습1", method: try1 },
-];
