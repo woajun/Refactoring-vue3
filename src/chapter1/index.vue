d92edbff8b9bf26b49f4e76d2a9a3f08fc6bd0c8
juni <woajun@naver.com>
2022-06-26 17:41:25 +0900

    ch1 add solution1, solution2

--- src/chapter1/index.vue
+++ src/chapter1/index.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Try1Vue from "./try1";
 import Example from "./example";
+import Solution1 from "./solution1";
+import Solution2 from "./solution2";
 import Tab from "@/components/Tab.vue";
 const subjects = {
   예제: Example,
+  중간풀이: Solution1,
+  풀이: Solution2,
   연습1: Try1Vue,
 };
 </script>
---
+++ src/chapter1/solution2/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Tester from "@/assets/js/testerClass.js";
+import TestBoard from "@/components/TestBoard.vue";
+import statement from "./statement.js";
+import i from "../condition/invoices";
+import p from "../condition/plays";
+import expectStatement from "../example/statement";
+
+const tester = new Tester(statement(i, p), expectStatement(i, p));
+</script>
+<template>
+  <TestBoard :tester="tester" />
+</template>
