23382d70f65f38234ec9f24b00fc0fde3eae56e4
juni <woajun@naver.com>
2022-06-26 17:27:35 +0900

    ch1 index add Example

--- src/chapter1/index.vue
+++ src/chapter1/index.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Try1Vue from "./try1";
+import Example from "./example";
 import Tab from "@/components/Tab.vue";
 const subjects = {
+  예제: Example,
   연습1: Try1Vue,
 };
 </script>
 <template>
   <Tab :chapters="subjects" />
-  <Try1Vue />
 </template>
