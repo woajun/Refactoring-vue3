4c608ddaaa0671c08f6ad32fbb6cc87bf930a45a
juni <woajun@naver.com>
2022-06-26 17:25:11 +0900

    use Tab in chapter1 index

--- src/chapter1/index.vue
+++ src/chapter1/index.vue
@@ -1,22 +1,11 @@
 <script setup>
-import example from "./example/statement.js";
-import solution1 from "./solution1/statement.js";
-import solution2 from "./solution2/statement.js";
 import Try1Vue from "./try1";
-import Chapter from "../components/Chapter.vue";
-import Executor from "../executor";
-import i from "./condition/invoices";
-import p from "./condition/plays.js";
-
-const sections = {
-  "마틴 파울러": [
-    new Executor("예제", example(i, p), example(i, p)),
-    new Executor("중간풀이", solution1(i, p), example(i, p)),
-    new Executor("풀이", solution2(i, p), example(i, p)),
-  ],
+import Tab from "@/components/Tab.vue";
+const subjects = {
+  연습1: Try1Vue,
 };
 </script>
 <template>
-  <chapter :sections="sections"></chapter>
+  <Tab :chapters="subjects" />
   <Try1Vue />
 </template>
