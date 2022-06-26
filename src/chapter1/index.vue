f284c8ecf17021c28151dbac1e822c91ef450b7e
juni <woajun@naver.com>
2022-06-26 16:52:44 +0900

    새로고침, 자동테스트 기능 1

--- src/chapter1/index.vue
+++ src/chapter1/index.vue
@@ -3,6 +3,7 @@ import example from "./example/statement.js";
 import solution1 from "./solution1/statement.js";
 import solution2 from "./solution2/statement.js";
 import try1 from "./try1/statement.js";
+import Try1Vue from "./try1";
 import Chapter from "../components/Chapter.vue";
 import Executor from "../executor";
 import i from "./condition/invoices";
@@ -19,4 +20,7 @@ const sections = {
 </script>
 <template>
   <chapter :sections="sections"></chapter>
+  <Try1Vue
+    :executor="new Executor('연습1', try1(i, p), example(i, p))"
+  ></Try1Vue>
 </template>
---
+++ src/chapter1/try1/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from "vue";
+import Executor from "../../executor";
+
+defineProps({ executor: Executor });
+</script>
+<template>
+  <div>
+    {{ executor.name }}
+    {{ executor.expect }}
+    {{ executor.result }}
+  </div>
+</template>
